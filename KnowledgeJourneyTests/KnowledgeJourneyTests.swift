2a249089722d17bd1923a5318579b8e267c0826d
suhaybabsi <suhaybal-absi@192.168.1.10>
2018-08-20 19:04:38 +0300

    tests

---
+++ KnowledgeJourneyTests/KnowledgeJourneyTests.swift
@@ -0,0 +1,38 @@
+//
+//  KnowledgeJourneyTests.swift
+//  KnowledgeJourneyTests
+//
+//  Created by Suhayb Al-Absi on 8/20/18.
+//  Copyright © 2018 Suhayb Al-Absi. All rights reserved.
+//
+
+import XCTest
+
+class KnowledgeJourneyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        assert(true, "Testing assert in sample unit test")
+    }
+    
+    func testPerformanceExample() {
+        
+        
+        self.measure {
+            for i in 0 .. 100 {
+                print("Hello, World !")
+            }
+        }
+    }
+    
+}
